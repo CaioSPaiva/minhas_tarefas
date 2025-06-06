a9e4b349e817a9e9f88a64bab04b31acc6378940
CaioSPaiva <caiopaiva134@gmail.com>
2025-06-06 18:34:20 -0300

    editar ou remover uma tarefa

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,18 +1,20 @@
 import { useSelector } from 'react-redux'
 
-import Tarefa from '../../Tarefa'
+import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
-  const { tarefas } = useSelector((state: RootReducer) => state)
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
+
   return (
     <Container>
       <p>2 tarefas marcadas como &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
       <ul>
-        {tarefas.map((t) => (
+        {itens.map((t) => (
           <li key={t.titulo}>
             <Tarefa
+              id={t.id}
               descricao={t.descricao}
               titulo={t.titulo}
               status={t.status}
