2006b4da927b157da254864237e465fb53cd3ca0
CaioSPaiva <caiopaiva134@gmail.com>
2025-06-12 18:26:30 -0300

    navegando entre rotas

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import BarraLateral from '../../containers/BarraLateral'
+import ListaDeTarefas from '../../containers/ListaDeTarefas'
+import BotaoAdiconar from '../../components/BotaoAdicionar'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+    <BotaoAdiconar />
+  </>
+)
+
+export default Home
